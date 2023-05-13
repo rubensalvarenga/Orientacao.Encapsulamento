daf8750578305e7a3de95d35ea10ebffd92b1bb0
rubensalvarenga <rubens_alvarenga@hotmail.com>
2023-05-13 11:02:57 -0300

    atualiza

---
+++ Orientacao.Encapsulamento/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Orientacao.Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "João";
+            pessoa.Idade = 25;
+
+            Console.WriteLine("Nome: " + pessoa.Nome);
+            Console.WriteLine("Idade: " + pessoa.Idade);
+        }
+
+        class Pessoa
+        {
+            private string nome;
+            private int idade;
+
+            public string Nome
+            {
+                get { return nome; }
+                set { nome = value; }
+            }
+
+            public int Idade
+            {
+                get { return idade; }
+                set { idade = value; }
+            }
+        }
+    }
+}
